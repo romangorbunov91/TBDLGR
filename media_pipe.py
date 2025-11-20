b48de13fd617dba661d2f0892868b86a95a621ed
ivanenko <ivanenko.stasy@gmail.com>
2025-11-20 15:52:17 +0500

    делённый mediapipe

--- media_pipe.py
+++ media_pipe.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 import os
-import mediapipe as mp
+# import mediapipe as mp
 
 
 def calculate_sharpness(frame):
@@ -62,60 +62,62 @@ def extract_sharpest_frames(video_path, output_folder, num_frames=40):
     return [os.path.join(output_folder, f"sharp_frame_{i+1:03d}.jpg") for i in range(num_frames)]
 
 
-def media_pipe(pictures, out_dir_name):
-    mp_drawing = mp.solutions.drawing_utils
-    mp_drawing_styles = mp.solutions.drawing_styles
-    mp_hands = mp.solutions.hands
+# def media_pipe(pictures, out_dir_name):
+#     mp_drawing = mp.solutions.drawing_utils
+#     mp_drawing_styles = mp.solutions.drawing_styles
+#     mp_hands = mp.solutions.hands
 
-    # Создаём папку для выходных изображений, если её нет
-    if not os.path.exists(out_dir_name):
-        os.makedirs(out_dir_name)
+#     # Создаём папку для выходных изображений, если её нет
+#     if not os.path.exists(out_dir_name):
+#         os.makedirs(out_dir_name)
 
-    # Инициализация модели MediaPipe Hands
-    with mp_hands.Hands(
-        static_image_mode=True,
-        max_num_hands=2,
-        min_detection_confidence=0.5) as hands:
+#     # Инициализация модели MediaPipe Hands
+#     with mp_hands.Hands(
+#         static_image_mode=True,
+#         max_num_hands=2,
+#         min_detection_confidence=0.5) as hands:
         
-        # Перебираем все изображения
-        for idx, file in enumerate(pictures):
-            # Читаем изображение
-            image = cv2.flip(cv2.imread(file), 1)
+#         # Перебираем все изображения
+#         for idx, file in enumerate(pictures):
+#             # Читаем изображение
+#             image = cv2.flip(cv2.imread(file), 1)
             
-            # Обрабатываем изображение с помощью MediaPipe
-            results = hands.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+#             # Обрабатываем изображение с помощью MediaPipe
+#             results = hands.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
             
-            if not results.multi_hand_landmarks:
-                continue
+#             if not results.multi_hand_landmarks:
+#                 continue
 
-            image_height, image_width, _ = image.shape
-            annotated_image = image.copy()
+#             image_height, image_width, _ = image.shape
+#             annotated_image = image.copy()
             
-            # Рисуем landmarks на изображении
-            for hand_landmarks in results.multi_hand_landmarks:
-                mp_drawing.draw_landmarks(
-                    annotated_image,
-                    hand_landmarks,
-                    mp_hands.HAND_CONNECTIONS,
-                    mp_drawing_styles.get_default_hand_landmarks_style(),
-                    mp_drawing_styles.get_default_hand_connections_style()
-                )
+#             # Рисуем landmarks на изображении
+#             for hand_landmarks in results.multi_hand_landmarks:
+#                 mp_drawing.draw_landmarks(
+#                     annotated_image,
+#                     hand_landmarks,
+#                     mp_hands.HAND_CONNECTIONS,
+#                     mp_drawing_styles.get_default_hand_landmarks_style(),
+#                     mp_drawing_styles.get_default_hand_connections_style()
+#                 )
             
-            # Сохраняем размеченное изображение в выходной папке
-            output_filename = os.path.join(out_dir_name, f"annotated_{idx+1}.png")
-            cv2.imwrite(output_filename, cv2.flip(annotated_image, 1))
-            print(f"Сохранено размеченное изображение: {output_filename}")
+#             # Сохраняем размеченное изображение в выходной папке
+#             output_filename = os.path.join(out_dir_name, f"annotated_{idx+1}.png")
+#             cv2.imwrite(output_filename, cv2.flip(annotated_image, 1))
+#             print(f"Сохранено размеченное изображение: {output_filename}")
 
 
 # Пример использования
 if __name__ == "__main__":
     # Путь к видео
-    video_path = '00b6a969f5d91a0c179252c471edfdbc.mp4'  # Путь к видеофайлу
+    
     output_folder = 'sharp_frames'  # Папка для сохранения самых резких кадров
     annotated_folder = 'annotated_images'  # Папка для сохранения размеченных изображений
 
-    # Извлекаем 40 самых резких кадров
-    sharp_frames = extract_sharpest_frames(video_path, output_folder)
+    folder_stock_path = "C:\\Users\\ivane\\OneDrive\\Documents\\итмо\\TBDLGR\\trimmed"  # Укажите путь к папке
 
-    # Прогоняем каждое изображение через media_pipe и сохраняем результат
-    media_pipe(sharp_frames, annotated_folder)
+    for root, dirs, files in os.walk(folder_stock_path):
+        for file in files:
+            sharp_frames = extract_sharpest_frames(file, output_folder)
+            # Прогоняем каждое изображение через media_pipe и сохраняем результат
+            # media_pipe(sharp_frames, annotated_folder)
