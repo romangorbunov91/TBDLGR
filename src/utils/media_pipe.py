ef243697ac3cc5fe9729ae4312f65bc6756483ca
Roman Gorbunov <romangorbunov91@yandex.ru>
2025-12-01 10:29:31 +0700

    Настроена разметка точек кисти.

--- src/utils/media_pipe.py
+++ src/utils/media_pipe.py
@@ -4,14 +4,10 @@ import mediapipe as mp
 import cv2
 
 
-def media_pipe(pictures, out_dir_name):
+def media_pipe(root, pictures, out_dir_name):
     mp_drawing = mp.solutions.drawing_utils
     mp_drawing_styles = mp.solutions.drawing_styles
     mp_hands = mp.solutions.hands
-
-    # Создаём папку для выходных изображений, если её нет
-    if not os.path.exists(out_dir_name):
-        os.makedirs(out_dir_name)
 
     # Инициализация модели MediaPipe Hands
     with mp_hands.Hands(
@@ -22,7 +18,8 @@ def media_pipe(pictures, out_dir_name):
         # Перебираем все изображения
         for idx, file in enumerate(pictures):
             # Читаем изображение
-            image = cv2.flip(cv2.imread(file), 1)
+            print(os.path.join(root, file))
+            image = cv2.flip(cv2.imread(os.path.join(root, file)), 1)
             
             # Обрабатываем изображение с помощью MediaPipe
             results = hands.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
@@ -44,7 +41,7 @@ def media_pipe(pictures, out_dir_name):
                 )
             
             # Сохраняем размеченное изображение в выходной папке
-            output_filename = os.path.join(out_dir_name, f"annotated_{idx+1}.png")
+            output_filename = os.path.join(root.replace('frames', out_dir_name), f"frame_{idx:03d}.jpg")
             cv2.imwrite(output_filename, cv2.flip(annotated_image, 1))
             print(f"Сохранено размеченное изображение: {output_filename}")
 
@@ -53,12 +50,20 @@ def media_pipe(pictures, out_dir_name):
 if __name__ == "__main__":
     # Путь к видео
     
-    output_folder = 'sharp_frames'  # Папка для сохранения самых резких кадров
-    annotated_folder = 'annotated_images'  # Папка для сохранения размеченных изображений
-
-    folder_stock_path = "C:\\Users\\ivane\\OneDrive\\Documents\\итмо\\TBDLGR\\trimmed"  # Укажите путь к папке
+    in_folder = './datasets/Bukva/frames'
+    out_folder = 'mediapipe_frames'  # Папка для сохранения размеченных изображений
+    '''
     pictures = []
-    for root, dirs, files in os.walk(output_folder):
+    for root, dirs, files in os.walk(in_folder):
         for file in files:
             print(file)
-    # media_pipe(file, annotated_folder)
+            
+    '''            
+    for root, dirs, files in os.walk(in_folder):
+        new_root = root.replace('frames', out_folder)
+        if not os.path.exists(new_root):
+            os.makedirs(new_root)  
+        
+        #for file in files:
+        #full_path = os.path.join(root, file)
+        media_pipe(root, files, out_folder)
